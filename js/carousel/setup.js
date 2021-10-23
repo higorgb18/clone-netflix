83825be06d80a5f71e4d0c84ae7b5e564fde4b75
Higor Gonçalves Brandão <higorb2000@gmail.com>
2021-10-23 17:22:16 -0300

    adicionando página de mais informações

--- js/carousel/setup.js
+++ js/carousel/setup.js
@@ -14,3 +14,20 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+$('.owl-carousel-main').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
